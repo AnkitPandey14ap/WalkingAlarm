21ef14b83484d37e98909c2e893664f066acf942
AnkitPandey14ap <ankit.pandey14ap@gmail.com>
2017-08-19 18:47:54 +0530

    Alarm Details

---
+++ app/src/main/java/com/example/krishna/walkingalarm/ActivityPackage/AlarmDetails.java
@@ -0,0 +1,46 @@
+package com.example.krishna.walkingalarm.ActivityPackage;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.example.krishna.walkingalarm.R;
+
+public class AlarmDetails extends AppCompatActivity {
+
+/*
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    return true;
+                case R.id.navigation_notifications:
+                    return true;
+            }
+            return false;
+        }
+
+    };*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_details);
+
+/*
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+*/
+
+
+    }
+
+}
